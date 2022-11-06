ede5387cd1ecc7129f4fa46d25cb84b7901ffaa8
Charles Powell <charlie.powell1@outlook.com>
2022-11-06 20:09:09 +0000

    Custom class for status action container

---
+++ app/train_vis_mobile/lib/view/pages/status/status_action_container.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
+import 'package:train_vis_mobile/view/routes/routes.dart';
+import 'package:train_vis_mobile/view/theme/data/my_colors.dart';
+import 'package:train_vis_mobile/view/theme/data/my_sizes.dart';
+import 'package:train_vis_mobile/view/theme/data/my_text_styles.dart';
+import 'package:train_vis_mobile/view/theme/widgets/my_text_button.dart';
+import 'package:train_vis_mobile/view/widgets/bordered_container.dart';
+
+/// Widget that displays the status of the vehicle along with any action that
+/// should be taken.
+class StatusActionContainer extends StatelessWidget {
+  // MEMBER VARIABLES //
+  final String vehicleID; // ID of vehicle being displayed
+
+  // ///////////////// //
+  // CLASS CONSTRUCTOR //
+  // ///////////////// //
+
+  const StatusActionContainer({
+    super.key,
+    required this.vehicleID,
+  });
+
+  // //////////// //
+  // BUILD METHOD //
+  // //////////// //
+
+  @override
+  Widget build(BuildContext context) {
+    return BorderedContainer(
+      borderColor: MyColors.negative,
+      backgroundColor: MyColors.negativeAccent,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // /////////////////// //
+          // CONFORMANCE MESSAGE //
+          // /////////////////// //
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Icon(
+                FontAwesomeIcons.circleExclamation,
+                size: MySizes.mediumIconSize,
+                color: MyColors.red,
+              ),
+              SizedBox(width: MySizes.spacing),
+              Text(
+                "No non-conformances present.",
+                style: MyTextStyles.headerText3,
+              ),
+            ],
+          ),
+
+          const SizedBox(height: MySizes.spacing),
+
+          // /////////////////////// //
+          // CONFORMANCE DESCRIPTION //
+          // /////////////////////// //
+
+          const Text(
+            "There are currently 5 non-conformances present on this vehicle.",
+            style: MyTextStyles.bodyText1,
+            textAlign: TextAlign.center,
+          ),
+
+          const SizedBox(height: MySizes.spacing),
+
+          // //////////////////////// //
+          // START REMEDIATION BUTTON //
+          // //////////////////////// //
+
+          MyTextButton.custom(
+            backgroundColor: MyColors.negative,
+            borderColor: MyColors.negative,
+            textColor: MyColors.antiPrimary,
+            text: "Start Remediation",
+            onPressed: () {
+              // navigating to status
+              context.pushNamed(
+                Routes.remediate,
+                params: {"vehicleID": vehicleID},
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
